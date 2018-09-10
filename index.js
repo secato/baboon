e588556952131b3ed455d78bc740aa9155c1c1ac
Felipe Secato <felipe.secato@gmail.com>
2018-09-10 10:40:12 -0300

    changed greeting

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = express()
 
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there' })
+    res.send({ bye: 'buddy' })
 })
 
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => { console.log('Server running') })
+app.listen(PORT, () => { console.log('Server running') })       
