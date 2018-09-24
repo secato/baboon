24679c4e87824ec751a2e7753fa0b29a8b20663a
Felipe Secato <felipe.secato@gmail.com>
2018-09-24 10:01:10 -0300

    basic components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,9 +4,10 @@ import { connect } from 'react-redux'
 import * as actions from '../actions'
 
 import Header from './Header'
+import Landing from './Landing'
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
-const Landing = () => <h2>Landing</h2>
+// const Landing = () => <h2>Landing</h2>
 
 class App extends Component {
 
@@ -16,16 +17,16 @@ class App extends Component {
 
     render() {
         return (
-            <div className="container">
-                <BrowserRouter>
-                    <div>
-                        <Header />
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <div className="container">
                         <Route exact path="/" component={Landing} />
                         <Route exact path="/surveys" component={Dashboard} />
                         <Route path="/surveys/new" component={SurveyNew} />
                     </div>
-                </BrowserRouter>
-            </div>
+                </div>
+            </BrowserRouter>
         )
     }
 }
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+
+
+
 
 class Header extends Component {
+
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                break
+            case false:
+                return (<a href="/auth/google">Login With Google</a>)
+            default:
+                return <a href="/api/logout">Logout</a>
+        }
+    }
+
     render() {
         return (
             <nav>
                 <div className="nav-wrapper blue darken-3">
-                    <a href="#" className="brand-logo">Logo</a>
-                    <ul id="nav-mobile" className="right hide-on-med-and-down">
-                        <li><a href="#">Login With Google</a></li>
-                    </ul>
+                    <div className="container">
+                        <Link
+                            to={this.props.auth ? '/surveys' : '/'}
+                            className="brand-logo">
+                            Emaily
+                        </Link>
+                        <ul id="nav-mobile" className="right">
+                            <li>
+                                {this.renderContent()}
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
         )
     }
 }
 
-export default Header
+function mapStateToProps({ auth }) {
+    return { auth }
+}
+
+export default connect(mapStateToProps)(Header)
