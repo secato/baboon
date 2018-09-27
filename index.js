c36da7fbb217a9075b2c78392703d1fcf23cdd83
Felipe Secato <felipe.secato@gmail.com>
2018-09-27 19:35:53 -0300

    heroku post build and express default route for react and static build

--- index.js
+++ index.js
@@ -29,5 +29,17 @@ app.use(passport.session())
 require('./routes/authRoutes')(app)
 require('./routes/billingRoutes')(app)
 
+if (process.env.NODE_ENV === 'production') {
+  // serve up production assets like main.js and main.css
+  // public static folder
+  app.use(express.static('client/build'))
+
+  // serve index.html if is not a recognized express route (react route for example)
+  const path = require('path')
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => { console.log(`Server running on port ${PORT}`) })
