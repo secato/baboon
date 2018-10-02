3ffddfe8901b4fe1c1eb862e1b2fe39194abe925
Felipe Secato <felipe.secato@gmail.com>
2018-10-02 10:43:49 -0300

    dynamic url setup for survey

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,8 +7,13 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
 const Survey = mongoose.model('surveys')
 
 module.exports = app => {
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+  app.get('/api/surveys/thanks', (req, res) => {
+    res.send('Thanks for voting!')
+  })
+
+  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     // destructuring to get the survey data
+
     const { title, subject, body, recipients } = req.body
 
     const survey = new Survey({
@@ -20,9 +25,17 @@ module.exports = app => {
       dateSent: Date.now()
     })
 
-    const mailer = new Mailer(survey, surveyTemplate(survey))
-    mailer.send()
-      .then(result => console.log(result))
-      .catch(err => console.error(err))
+    try {
+      const mailer = new Mailer(survey, surveyTemplate(survey))
+      await mailer.send()
+      await survey.save()
+
+      // subtract 1 credit from the user
+      req.user.credits -= 1
+      const user = await req.user.save()
+      res.send(user)
+    } catch (err) {
+      res.status(422).send(err)
+    }
   })
 }
